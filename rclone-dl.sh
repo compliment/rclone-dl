d0a25f26f735f6116ba81f20803213cb6e3176f7
compliment <elbasuv@gmail.com>
2017-06-30 17:25:19 +0530

    ;; removed

--- rclone-dl.sh
+++ rclone-dl.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-  
-      
             echo "Installing rclone Linux AMD64"
             rm -rf rclone*.zip
-            bash <(curl -Ss https://raw.githubusercontent.com/DEDS3C/rclone-dl/master/rclone.sh) ncw/rclone linux-amd64 zip
+            bash <(curl -Ss https://raw.githubusercontent.com/compliment/rclone-dl/master/rclone.sh) ncw/rclone linux-amd64 zip
             unzip -o rclone-*-linux-amd64.zip
             echo "Assuming root"
             
@@ -12,6 +10,5 @@
             cp rclone /usr/bin/
             chown $USER:$USER /usr/bin/rclone
             chmod 755 /usr/bin/rclone
-            ;;
-      
-esac
+             echo "Assuming finished installing"
+      esac
